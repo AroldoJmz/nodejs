eceee11c1bc9f607c32a611f97488143e0b094db
unknown <1200198@alumno.um.edu.mx>
2023-03-28 14:06:08 -0600

    Actualización de archivos 2.0

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const http = require('http');
 const PORT = 3000;
 
+const values = [
+  'HGV-1562',
+  'SPA-9331',
+  'ALT-4597',
+  'SSS-3898',
+  'IMS-8080',
+  'PNG-1234' 
+]
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.setHeader('Content-Type', 'application/json');
+  res.json(values);
 });
+
+
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
