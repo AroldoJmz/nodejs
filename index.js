9b7d21a33db7460f2099d1631dc544fc37c0a79d
unknown <1200198@alumno.um.edu.mx>
2023-03-31 13:50:58 -0600

    Modificación package.json

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const http = require('http');
 const PORT = 3000;
 
-const value = 'Prueba de API';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(value);
+  res.end('Hello World!');
 });
 
 
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log('Server running at http://localhost:${PORT}/');
 });
